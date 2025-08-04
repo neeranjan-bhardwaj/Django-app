6f3f8e857efb158f8ecfd2aeacf4554563dbaaf4
neeranjan-bhardwaj <neeranjan5116@gmail.com>
2025-08-04 16:13:02 +0530

    created a basic Django app structure

--- Django_app/anime_view/views.py
+++ Django_app/anime_view/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    temp=loader.get_template('index.html')
+    objects = [
+        {
+            'name': 'Naruto',
+            'id': 1
+        },
+        {
+            'name': 'One Piece',
+            'id': 2
+        },
+        {
+            'name': 'Attack on Titan',
+            'id': 3
+        },
+        {
+            'name': 'My Hero Academia',
+            'id': 4
+        },
+    ]
+constx={
+        'x':objects
+    }
+    # print(objects)
+    return HttpResponse(temp.render(constx,request))
+
+def y(request, id):
+    template = loader.get_template('y.html')
+    return HttpResponse(template.render())
